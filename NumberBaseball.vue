e0d2a07f0afd09083066d7a415e946e4d8523118
hyemin <hm_stom@naver.com>
2021-07-22 00:14:56 +0900

    #setting number 1

---
+++ NumberBaseball.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h1>{{result}}</h1>
+      <form v-on:submit="onSubmitForm">
+          <input type="number" ref="answer" v-model="value" placeholder="Enter a number" />
+      </form>
+
+      <div>시도 : {{}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            value : '',
+            result : '',
+        }
+    },
+    methods : {
+        onSubmitForm(e) {
+            e.preventDefault();
+        }
+    },
+};
+
+</script>
+
+<style>
+
+</style>
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-
+import NumberBaseball from "./NumberBaseball";
 
 // Vue instance 생성 mount -> el 역할을 함
-new Vue().$mount('root');
+new Vue(NumberBaseball).$mount('#root');
