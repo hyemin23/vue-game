45e9859ce8be4a7dc9b5cee0110b4209449a458e
hyemin <hm_stom@naver.com>
2021-07-22 00:29:02 +0900

    #

--- NumberBaseball.vue
+++ NumberBaseball.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
       <h1>{{result}}</h1>
-      <form v-on:submit="onSubmitForm">
+      <form @submit.prevent="onSubmitForm">
           <input type="number" ref="answer" v-model="value" placeholder="Enter a number" />
+          <button type="submit">입력</button>
       </form>
 
-      <div>시도 : {{}}</div>
+      <div>시도 : {{ tries.length }}</div>
+      <ul>
+          <li v-for="t in tries" v-bind:key="t.try">
+              <div>
+              {{ t.try }}
+              </div>
+              <div>
+                  {{ t.result }}
+              </div>
+          </li>
+      </ul>
   </div>
 </template>
 
@@ -13,13 +24,19 @@
 export default {
     data(){
         return {
+            tries:[],
             value : '',
             result : '',
         }
     },
     methods : {
-        onSubmitForm(e) {
-            e.preventDefault();
+        onSubmitForm() {
+            this.tries.push({
+                try : this.value
+                ,result : '홈런'
+            });
+            this.value ='';
+            this.$refs.answer.focus();
         }
     },
 };
